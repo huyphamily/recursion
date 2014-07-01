fb55ad03647b2bbf2b4449701007aa66c1e70979
Huy Pham <huyphamily@gmail.com>
2014-06-30 22:28:42 -0700

    implemented stringifyJSON to take values and arrays.

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,4 +4,16 @@
 // but you don't so you're going to write it from scratch:
 var stringifyJSON = function(obj) {
   // your code goes here
+  if(obj === null) {
+  	return 'null';
+  } else if (Array.isArray(obj)) {
+  	for (var i = 0; i < obj.length; i++) {
+  		obj[i] = stringifyJSON(obj[i]);
+  	}
+  	return "[" + obj + "]";
+  } else if (typeof(obj) === "string") {
+  	return '"' + obj.toString() + '"';
+  } else {
+  	return obj.toString();
+  }
 };
