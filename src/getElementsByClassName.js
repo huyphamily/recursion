e608457222cdcce64a54a40faa480a9e38830353
huyphamily <huyphamily@gmail.com>
2014-09-24 03:44:08 -0700

    Revert "deleted solution"
    This reverts commit 418db8c11ae7129cea4faceef438549e2ceb7810.

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -6,5 +6,24 @@
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className){
   // your code here
+  var result = [];
 
+  function check (node) {
+    if (node !== undefined) {
+  		if (node.length) {
+  			for (var i = 0; i < node.length; i++) {
+  				check(node[i]);
+  			}
+  		} else {
+  			if (node.classList) {
+  				if (node.classList.contains(className)) {
+  					result.push(node);
+  				}
+  				check(node.childNodes);
+  			}
+ 	 	}
+ 	 }
+ 	}
+  check(document.body);
+  return result;
 };
